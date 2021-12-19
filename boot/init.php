46b04c48306c0b7b07f068932517efcc9b0dedd9
Chris Williams <spider.mane.web@gmail.com>
2021-12-19 18:26:13 -0500

    container configuration in seperate file

--- boot/init.php
+++ boot/init.php
@@ -4,10 +4,10 @@ defined('ABSPATH') || exit;
 
 $root = dirname(__DIR__, 1);
 
-require __DIR__ . '/functions.php';
-require __DIR__ . '/constants.php';
-
 // composer autoload
 if (file_exists($autoload = $root . '/vendor/autoload.php')) {
     require $autoload;
 }
+
+require __DIR__ . '/functions.php';
+require __DIR__ . '/constants.php';
--- config/app.php
+++ config/app.php
@@ -1,7 +1,5 @@
 <?php
 
-use Leonidas\Library\Core\Config\ConfigReflector;
-
 return [
 
     /**
@@ -16,51 +14,9 @@ return [
      * reusable between projects.
      */
     'modules' => [
+        PseudoVendor\PseudoPlugin\Modules\AdminAssets::class,
         PseudoVendor\PseudoPlugin\Modules\Setup::class,
-        PseudoVendor\PseudoPlugin\Modules\ProvisionAdminAssets::class,
     ],
-
-    /**
-     *==========================================================================
-     * Services
-     *==========================================================================
-     *
-     * Services are pre-configured objects made available throughout your
-     * application via a dependency injection container. You can define
-     * individual services here in a manner that allows you to populate your
-     * container by iterating over them. Doing this cleanly from this context
-     * requires use of a container with introspection capabilities or a
-     * StaticProviderInterface, which is a simple static, library-independent
-     * service provider which contains the actual instantiation logic as well as
-     * a ConfigReflectorInterface instance which allows for providing the a set
-     * of options from the same configuration repository. Besides "id",
-     * "provider", and "args", the exact key=>value structure will depend on
-     * your container library of choice.
-     */
-    'services' => [
-        [
-            'id' => Leonidas\Library\Admin\Loaders\AdminNoticeCollectionLoaderInterface::class,
-            'provider' => Leonidas\Framework\Providers\AdminNoticeCollectionLoaderProvider::class,
-            'args' => ConfigReflector::map([
-                'prefix' => 'plugin.prefix'
-            ]),
-            'shared' => true,
-            'tags' => ['admin_notice_loader']
-        ]
-    ],
-
-    /**
-     *==========================================================================
-     * Providers
-     *==========================================================================
-     *
-     * Some DI containers support "service providers." These are typically self
-     * contained classes with all the logic required for inserting an entry into
-     * a container according to that container's capabilities. Because all
-     * necessary logic is encapsulated within them, library-specific providers
-     * are the cleanest solution to building your container.
-     */
-    'providers' => [],
 
     /**
      *==========================================================================
