77a122d7ae422083bed0810e95fac8636db1de00
Chris Williams <spider.mane.web@gmail.com>
2021-12-20 04:53:19 -0500

    AdminAssets example module using Leonidas Asset library

---
+++ src/Modules/AdminAssets.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace PseudoVendor\PseudoPlugin\Modules;
+
+use Leonidas\Contracts\Extension\ModuleInterface;
+use Leonidas\Contracts\Ui\Asset\ScriptCollectionInterface;
+use Leonidas\Contracts\Ui\Asset\StyleCollectionInterface;
+use Leonidas\Framework\Modules\AbstractAdminAssetProviderModule;
+use Leonidas\Library\Core\Asset\ScriptCollection;
+use Leonidas\Library\Core\Asset\StyleCollection;
+
+final class AdminAssets extends AbstractAdminAssetProviderModule
+{
+    protected function styles(): ?StyleCollectionInterface
+    {
+        return StyleCollection::with(
+            //
+        );
+    }
+
+    protected function scripts(): ?ScriptCollectionInterface
+    {
+        return ScriptCollection::with(
+            //
+        );
+    }
+}
